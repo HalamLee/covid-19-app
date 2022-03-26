51411349cd72ece1a21c2e0ccaeafce7e429a6ff
Lami <Halam.Lee.Lami@gmail.com>
2022-03-26 16:18:13 +0900

    add: Mark selected countries

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Global, css } from '@emotion/react';
 import { useState, useEffect } from 'react';
 import CountryList from './components/CountryList';
 import GlobalInfo from './components/GlobalInfo';
-import type { ResponseData } from './types';
+import type { ResponseData, Country } from './types';
 
 const App: React.FunctionComponent = () => {
   const [data, setData] = useState<ResponseData | undefined>(undefined);
+  const [activeCountries, setActiveCountries] = useState<Country[]>([]);
 
   const fetchData = async () => {
     const result = await fetch('https://api.covid19api.com/summary');
@@ -18,6 +19,23 @@ const App: React.FunctionComponent = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const onCountryClick = (country: Country) => {
+    const countryIndex = activeCountries.findIndex(
+      (activeCountry) => activeCountry.ID === country.ID
+    );
+
+    if (countryIndex > -1) {
+      // 이미 선택한 나라이기 때문에 인덱스 반환
+      const newActiveCountries = [...activeCountries];
+      newActiveCountries.splice(countryIndex, 1); // 선택한 나라 삭제
+
+      setActiveCountries(newActiveCountries);
+    } else {
+      // 선택했던 나라가 아니면 -1 반환 >> 선택한 나라로 추가
+      setActiveCountries([...activeCountries, country]); // 기존에 선택했던 나라 + 새로 선택한 나라
+    }
+  };
 
   return (
     <div>
@@ -38,7 +56,10 @@ const App: React.FunctionComponent = () => {
             newRecovered={data?.Global.NewRecovered}
           />
 
-          <CountryList countries={data.Countries} />
+          <CountryList
+            countries={data.Countries}
+            onItemClick={onCountryClick}
+          />
         </>
       ) : (
         'Loading...'
