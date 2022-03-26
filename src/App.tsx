14ed2f901668bb94075ee206c47523a192f1473d
Lami <Halam.Lee.Lami@gmail.com>
2022-03-26 13:54:32 +0900

    Modify:Create types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
 import { useState, useEffect } from 'react';
 import GlobalInfo from './components/GlobalInfo';
-
-type Country = {
-  Country: string;
-  CountryCode: string;
-  Date: string;
-  ID: string;
-  NewConfirmed: number;
-  NewDeaths: number;
-  NewRecovered: number;
-  Premium: unknown;
-  Slug: string;
-  TotalConfirmed: number;
-  TotalDeaths: number;
-  TotalRecovered: number;
-};
-
-type GlobalData = {
-  Date: string;
-  NewConfirmed: number;
-  NewDeaths: number;
-  NewRecovered: number;
-  TotalConfirmed: number;
-  TotalDeaths: number;
-  TotalRecovered: number;
-};
-
-type ResponseData = {
-  Countries: Country[];
-  Date: string;
-  Global: GlobalData;
-  ID: string;
-  Message: string;
-};
+import type { ResponseData } from './types';
 
 const App: React.FunctionComponent = () => {
   const [data, setData] = useState<ResponseData | undefined>(undefined);
