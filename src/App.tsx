5226d2e34b38c0244d0c7ab57c58ff2d7f0c1a95
Lami <Halam.Lee.Lami@gmail.com>
2022-03-26 11:35:23 +0900

    Add:create types for server response

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,58 @@
+import { useState, useEffect } from 'react';
+
+type Country = {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: unknown;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+};
+
+type GlobalData = {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+};
+
+type ResponseData = {
+  Countries: Country[];
+  Date: string;
+  Global: GlobalData;
+  ID: string;
+  Message: string;
+};
+
 const App: React.FunctionComponent = () => {
-  return <div></div>;
+  const [data, setData] = useState<ResponseData | undefined>(undefined);
+
+  const fetchData = async () => {
+    const result = await fetch('https://api.covid19api.com/summary');
+    const data: ResponseData = await result.json();
+
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Global Covid-19 data</h1>
+    </div>
+  );
 };
 
 export default App;
